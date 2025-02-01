d757509ec1c5c0b17495fdf78ac63d90d20e231f
beejh02 <joon0448@naver.com>
2025-02-01 19:55:07 +0900

    .

---
+++ practice/src/main/java/com/beejh02/practice/PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.beejh02.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
