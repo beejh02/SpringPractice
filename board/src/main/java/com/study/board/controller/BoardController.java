5e4c005a20400ad468b31f130bab3a361ca53402
beejh02 <joon0448@naver.com>
2025-02-04 23:20:53 +0900

    .

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/write")
+    public String boardWriteForm() {
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardwritePro(String title, String content) {
+        
+        System.out.println("제목 : "+ title);
+        System.out.println("내용 : "+ content);
+
+        return "";
+    }
+}
