26997495e22f78c8e0c678ca669b275fdd9fff21
beejh02 <joon0448@naver.com>
2025-02-10 00:10:42 +0900

    securityconfig 작성

--- demo/src/main/java/dev/beejh02/demo/config/SecurityConfig.java
+++ demo/src/main/java/dev/beejh02/demo/config/SecurityConfig.java
@@ -2,16 +2,28 @@ package dev.beejh02.demo.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configurable
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
-    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+            .requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+
+        .authenticationProvider(authenticationProvider)
+        .httpBasic();
         return http.build();
     }
 
