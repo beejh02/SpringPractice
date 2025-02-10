351e85406d8f32920cbd329bf10deabd7de0be07
beejh02 <joon0448@naver.com>
2025-02-10 14:48:11 +0900

    불필요한 주석 제거

--- jwt/src/main/java/com/study/jwt/jwt/JWTUtil.java
+++ jwt/src/main/java/com/study/jwt/jwt/JWTUtil.java
@@ -22,23 +22,19 @@ public class JWTUtil {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
     }
 
-    // 토큰에서 username 추출
     public String getUsername(String token) {
         return getClaims(token).get("username", String.class);
     }
 
-    // 토큰에서 role 추출
     public String getRole(String token) {
         return getClaims(token).get("role", String.class);
     }
 
-    // 토큰 만료 여부 확인
     public Boolean isExpired(String token) {
         Date expiration = getClaims(token).getExpiration();
         return expiration != null && expiration.before(new Date());
     }
 
-    // JWT 생성
     public String createJwt(String username, String role, Long expiredMs) {
         return Jwts.builder()
                 .claim("username", username)
@@ -49,7 +45,6 @@ public class JWTUtil {
                 .compact();
     }
 
-    // JWT에서 Claims 추출
     private Claims getClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(secretKey)
