cdfe597af6c520fe4f3e1df7e814a2416838caa4
beejh02 <joon0448@naver.com>
2025-02-18 20:53:02 +0900

    빠진 의존성 추가 + security-config작성

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.LoginRequest;
+
 // import com.example.demo.util.JwtUtil;/
 
 import lombok.RequiredArgsConstructor;
