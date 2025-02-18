69b2027f06d2aafcc4f703e08969961b69f5356a
beejh02 <joon0448@naver.com>
2025-02-18 21:01:35 +0900

    user.java도 builder적용

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -6,12 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name = "users")
 @Entity
@@ -28,4 +27,11 @@ public class User {
 
     @Column(nullable = false)
     private String role = "USER";
+
+    @Builder
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
