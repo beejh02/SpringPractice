5d3fedb4bdfe89d5a84fada9771efe57bc823d21
beejh02 <joon0448@naver.com>
2025-02-18 20:23:11 +0900

    entity & repo & service 작성

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(String username, String password) {
+        if(userRepository.findByUsername(username).isPresent()) throw new RuntimeException("이미 존재하는 사용자입니다.");
+
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+
+}
